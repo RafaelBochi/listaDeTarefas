81bd8b52e2182248500e037ee457f6b89953082e
RafaelBochi <rafaelbochibarros@gmail.com>
2022-10-16 17:50:32 -0300

    remoção listas

--- script.js
+++ script.js
@@ -1,83 +1,128 @@
-const btnAddBasicList = document.getElementById('addListBasic');
-const blockBasicList = document.getElementById('listsBasic');
-const blockBasicListDados = document.getElementById('basicListDados');
-const btnCreate = document.getElementById('createListBasic')
+const btnAddBasicList = document.querySelector('.addListBasic');
+const dadosBasicList = document.querySelector('.basicListDados');
 
-btnAddBasicList.addEventListener('click', openListBasic);
+    let inputTitleBasicList = document.querySelector('#titleInputBasic');
+    let inputDescripBasicList = document.querySelector('#descripInputBasic');
+    let inputDateBasicList = document.querySelector('#dateInputBasic');
+    
 
+const openDadosBasic = () => {
 
-let i = true;
-function openListBasic() {
-
-    if (i == true) {
-        blockBasicListDados.style.display = "flex";
-        i = false}
-    else {
-        blockBasicListDados.style.display = "none";
-        i = true
-    }
+    dadosBasicList.classList.toggle('open-close');
+    inputDateBasicList.value = "";
+    inputDescripBasicList.value = "";
+    inputTitleBasicList.value = "";
 }
 
-let inputTitleBasicList = document.getElementById('titleInputBasic');
-let inputDescripBasicList = document.getElementById('descripInputBasic');
-let inputDateBasicList = document.getElementById('dateInputBasic');
+btnAddBasicList.addEventListener("click", () => openDadosBasic());
 
-btnCreate.addEventListener('click', createBasicList);
+const basicLists = document.querySelector('#listsBasic')
+const btnCreateBasicList = document.querySelector('#createListBasic');
 
-function createBasicList() {
 
-    if (inputTitleBasicList.value == '') alert('Adicione um titulo');
-    else if (inputDescripBasicList.value == '') alert('Adicione uma descrição');
-    else if (inputDateBasicList.value == '') {
-         inputDateBasicList.value = "Sem data";
-        
-         blockBasicList.innerHTML = blockBasicList.innerHTML + `<div class="listBasic"> 
+const createBasicList = () => {
 
-         <p class="title">
-             <strong>a</strong>
-         </p>
-     
-         <p class="descrip">
-             s
-         </p>
-     
-         <div class="date">
-            <p>Sem data</p>
-             <span id="removeBasicList">Remover</span>  
-         </div>
-     
-     </div>
-         `
+    const divListBasic = document.createElement('div');
+divListBasic.classList.add('listBasic');
 
+    const actionsBasicList = document.createElement('div')
+    actionsBasicList.classList.add('actions');
+
+    const iconActionsBasciList = document.createElement('i');
+    iconActionsBasciList.classList.add('fa-solid');
+    iconActionsBasciList.classList.add('fa-ellipsis-vertical');
+    iconActionsBasciList.classList.add('fa-2x');
+
+    actionsBasicList.appendChild(iconActionsBasciList);
+
+    const divItensActions = document.createElement('div');
+    divItensActions.classList.add('itensActions');
+    divItensActions.classList.add('open-close');
+
+
+    const remove = document.createElement('div');
+    remove.classList.add('remove');
+
+    const textRemove = document.createElement('p');
+    textRemove.innerHTML = "Remover";
+
+    const iconRemove = document.createElement('i');
+    iconRemove.classList.add('fa-solid')
+    iconRemove.classList.add('fa-trash-can')
+
+    remove.addEventListener('click', () => removeBasicList(remove, divListBasic))
+
+    remove.appendChild(textRemove);
+    remove.appendChild(iconRemove);
+
+    const complete = document.createElement('div');
+    complete.classList.add('complete');
+
+    const textComplete = document.createElement('p');
+    textComplete.innerHTML = "Concluir";
+
+    const iconComplete = document.createElement('i');
+    iconComplete.classList.add('fa-solid')
+    iconComplete.classList.add('fa-check')
+
+    complete.appendChild(textComplete);
+    complete.appendChild(iconComplete);
+
+
+    divItensActions.appendChild(remove);
+    divItensActions.appendChild(complete);
+
+    actionsBasicList.appendChild(divItensActions)
+
+    
+    iconActionsBasciList.addEventListener('click', () => openItensActions(actionsBasicList))
+
+    const titleBasicList = document.createElement('h1');
+    titleBasicList.classList.add('title');
+    titleBasicList.innerHTML = inputTitleBasicList.value;
+
+    const decripBasicList = document.createElement('p');
+    decripBasicList.classList.add('descrip');
+    decripBasicList.innerHTML = inputDescripBasicList.value;
+
+    const dateBasicList = document.createElement('div');
+    dateBasicList.classList.add('date');
+    dateBasicList.innerHTML = inputDateBasicList.value;
+
+    divListBasic.appendChild(actionsBasicList)
+    divListBasic.appendChild(titleBasicList);
+    divListBasic.appendChild(decripBasicList);
+    divListBasic.appendChild(dateBasicList);
+
+    basicLists.appendChild(divListBasic);
+
+    btnAddBasicList.click();
+}
+
+const openItensActions = (actionsBasicList) => {
+    let btnActions = document.querySelectorAll('.actions');
+
+    for(let btnAction of btnActions) {
+        if (btnAction.isSameNode(actionsBasicList)){
+            btnAction.children[1].classList.toggle('open-close');
         }
+    }
+};
 
-    else {
-        blockBasicList.innerHTML = blockBasicList.innerHTML + `<div class="listBasic"> 
+const removeBasicList = (remove, divListBasic) => {
+    let lists = document.querySelectorAll('.remove');
 
-        <p class="title">
-            <strong>a</strong>
-        </p>
-    
-        <p class="descrip">
-            s
-        </p>
-    
-        <div class="date">
-           <p>${inputDateBasicList.value}</p>
-            <span id="removeBasicList">Remover</span>  
-        </div>
-    
-    </div>
-        `
+    for (let list of lists) {
+        if (list.isSameNode(remove)) {
+            divListBasic.remove();
+        }
+        
     }
 }
 
-const btnRemoveBasicList = document.getElementById('removeBasicList');
+btnCreateBasicList.addEventListener('click', () => createBasicList());
 
-let listsBasic = document.getElementsByClassName('listBasic');
 
-btnRemoveBasicList.addEventListener('click', removerListBasic);
 
-function removerListBasic(event) {
-    console.log(event)
-}
+
+
